fbb93f48b39e7cb6ec38021e913db961b18f3099
HungNC28 <nguyenchihung0123@gmail.com>
2024-01-27 09:53:10 +0700

    update

--- src/Redux/Actions/cartAction.js
+++ src/Redux/Actions/cartAction.js
@@ -1,43 +1,43 @@
 import { cartService } from "../Services/cartService";
 
 export const addToCart = async (data) => {
-  try {
-    await cartService.addToCart(data);
-  } catch (error) {
-    console.log(error);
-  }
+    try {
+        await cartService.addToCart(data);
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 export const getCart = async (userId) => {
-  try {
-    const result = await cartService.getCartByUserId(userId);
-    // console.log("result:", result);
-    return result.data;
-  } catch (error) {
-    console.log(error);
-  }
+    try {
+        const result = await cartService.getCartByUserId(userId);
+        // console.log("result:", result);
+        return result.data;
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 export const updateQuantity = async (data) => {
-  try {
-    await cartService.updateQuantity(data);
-  } catch (error) {
-    console.log(error);
-  }
+    try {
+        await cartService.updateQuantity(data);
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 export const deleteItem = async (userId, productId) => {
-  try {
-    await cartService.deleteItem(userId, productId);
-  } catch (error) {
-    console.log(error);
-  }
+    try {
+        await cartService.deleteItem(userId, productId);
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 export const resetCart = (dispatch) => {
-  try {
-    // dispatch(createAction(CART.RESET_CART));
-  } catch (error) {
-    console.log(error);
-  }
+    try {
+        // dispatch(createAction(CART.RESET_CART));
+    } catch (error) {
+        console.log(error);
+    }
 };
--- src/Redux/Services/baseService.js
+++ src/Redux/Services/baseService.js
@@ -1,37 +1,37 @@
 import Axios from "axios";
 import { serverPath } from "../path";
 export class baseService {
-  get = (url) => {
-    return Axios({
-      withCredentials: true,
-      url: `${serverPath}${url}`,
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-  };
+    get = (url) => {
+        return Axios({
+            withCredentials: true,
+            url: `${serverPath}${url}`,
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+    };
 
-  post = (url, model) => {
-    return Axios({
-      withCredentials: true,
-      url: `${serverPath}${url}`,
-      method: "POST",
-      data: model,
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-  };
+    post = (url, model) => {
+        return Axios({
+            withCredentials: true,
+            url: `${serverPath}${url}`,
+            method: "POST",
+            data: model,
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+    };
 
-  delete = (url) => {
-    return Axios({
-      withCredentials: true,
-      url: `${serverPath}${url}`,
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-  };
+    delete = (url) => {
+        return Axios({
+            withCredentials: true,
+            url: `${serverPath}${url}`,
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+    };
 }
