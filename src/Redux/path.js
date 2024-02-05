e4472b15e72795bc6817241bb5a065623a5b0f7d
HungNC28 <nguyenchihung0123@gmail.com>
2024-02-05 14:48:43 +0700

    update

--- src/Redux/path.js
+++ src/Redux/path.js
@@ -1,4 +1,3 @@
-export const serverPath = "http://localhost:5000";
-// export const serverPath = "https://tets-pied.vercel.app";
+// export const serverPath = "http://localhost:5000";
 
-// export const serverPath = "https://ecommerce-be-website.onrender.com";
+export const serverPath = "https://ecommerce-be-website.onrender.com";
